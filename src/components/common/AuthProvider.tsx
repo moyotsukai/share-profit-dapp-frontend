db2d8ebc435f426f8e9d29a64e24849290fe7aa4
moyotsukai <gyoshu.bowie@gmail.com>
2023-05-04 17:08:21 +0900

    clean: empty files

--- src/components/common/AuthProvider.tsx
+++ src/components/common/AuthProvider.tsx
@@ -1,2 +1,13 @@
 //if user is signed in, set userData global
 //if not, sign in
+
+const AuthProvider: React.FC = () => {
+
+  return (
+    <div>
+    </div>
+  )
+
+}
+
+export default AuthProvider
