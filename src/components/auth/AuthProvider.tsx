27b0535cf9b611c08703c2f7c685a921b9bf2420
setouchi-h <setouchi.kazuki@gmail.com>
2023-05-22 01:47:54 +0900

    get amount

--- src/components/auth/AuthProvider.tsx
+++ src/components/auth/AuthProvider.tsx
@@ -1,22 +1,21 @@
-import { connectToMetaMask } from "@/models/auth/connectToMetaMask"
-import { signIn } from "@/models/auth/signIn"
-import { ethereum } from "@/models/ethereum/ethereum"
-import { asyncTask } from "@/utils/asyncTask"
-import React, { useEffect, useState } from "react"
-import Button from "../ui/Button"
-import { useUserState } from "@/states/userState"
-import LoadingCircle from "../ui/LoadingCircle"
+import { connectToMetaMask } from "@/models/auth/connectToMetaMask";
+import { signIn } from "@/models/auth/signIn";
+import { ethereum } from "@/models/ethereum/ethereum";
+import { asyncTask } from "@/utils/asyncTask";
+import React, { useEffect, useState } from "react";
+import Button from "../ui/Button";
+import { useUserState } from "@/states/userState";
+import LoadingCircle from "../ui/LoadingCircle";
 
 type Props = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 const AuthProvider: React.FC<Props> = ({ children }) => {
-
-  const [user, setUser] = useUserState()
-  const [message, setMessage] = useState<string>("")
-  const [isButtonEnabled, setIsButtonEnabled] = useState<boolean>(true)
-  const [isButtonLoading, setIsButtonLoading] = useState<boolean>(false)
+  const [user, setUser] = useUserState();
+  const [message, setMessage] = useState<string>("");
+  const [isButtonEnabled, setIsButtonEnabled] = useState<boolean>(true);
+  const [isButtonLoading, setIsButtonLoading] = useState<boolean>(false);
 
   useEffect(() => {
     if (user) {
@@ -24,76 +23,68 @@ const AuthProvider: React.FC<Props> = ({ children }) => {
       //do nothing
     } else {
       if (ethereum) {
-        const address = ethereum.selectedAddress
+        const address = ethereum.selectedAddress;
         if (address) {
           //connected, sign in
           asyncTask(async () => {
-            const authenticatedUser = await signIn({ address: address })
-            setUser(authenticatedUser)
-          })
+            const authenticatedUser = await signIn({ address: address });
+            setUser(authenticatedUser);
+          });
         } else {
           //not connected, user needs to connect and sign in manually
-          setUser(null)
+          setUser(null);
         }
       } else {
         //user needs to install MetaMask
-        setMessage("Please install MetaMask")
-        setUser(null)
+        setMessage("Please install MetaMask");
+        setUser(null);
       }
     }
-  }, [])
+  }, []);
 
   const onClickConnect = async () => {
-    setIsButtonEnabled(false)
-    setIsButtonLoading(true)
+    setIsButtonEnabled(false);
+    setIsButtonLoading(true);
 
-    const address = await connectToMetaMask()
+    const address = await connectToMetaMask();
     if (address) {
-      const user = await signIn({ address: address })
-      setUser(user)
+      const user = await signIn({ address: address });
+      setUser(user);
     } else {
-      setUser(null)
+      setUser(null);
     }
 
-    setIsButtonEnabled(true)
-    setIsButtonLoading(false)
-  }
+    setIsButtonEnabled(true);
+    setIsButtonLoading(false);
+  };
 
   return (
     <React.Fragment>
-      {user === undefined
-        ? (
-          <div>
-            <p>Loading</p>
-            <LoadingCircle />
-          </div>
-        )
-        : (
-          <React.Fragment>
-            {message && (
-              <p>
-                {message}
-              </p>
-            )}
-            {user
-              ?
-              children
-              : (
-                <div>
-                  <Button
-                    onClick={onClickConnect}
-                    isEnabled={isButtonEnabled}
-                    isLoading={isButtonLoading}
-                  >
-                    Connect with MetaMask
-                  </Button>
-                </div>
-              )}
-          </React.Fragment>
-        )}
+      {user === undefined ? (
+        <div>
+          <p>Loading</p>
+          <LoadingCircle />
+        </div>
+      ) : (
+        <React.Fragment>
+          {message && <p>{message}</p>}
+          {user ? (
+            children
+          ) : (
+            <div>
+              <Button
+                onClick={onClickConnect}
+                isEnabled={isButtonEnabled}
+                isLoading={isButtonLoading}
+              >
+                Connect Metamask
+              </Button>
+            </div>
+          )}
+        </React.Fragment>
+      )}
     </React.Fragment>
-  )
+  );
+};
 
-}
-
-export default AuthProvider
+export default AuthProvider;
--- src/components/pages/IndexPage.tsx
+++ src/components/pages/IndexPage.tsx
@@ -12,83 +12,143 @@ import { PATHS } from "./paths";
 import { useFetchEffect } from "@/models/project/useFetchEffect";
 import { getProjectsWhere } from "@/models/firestore/getProjectsWhere";
 import { KEYS } from "@/models/firestore/keys";
+import {
+  useContractRead,
+  useContractWrite,
+  usePrepareContractWrite,
+} from "wagmi";
+import accountAbi from "../../../constants/Account.json";
+import { ethers } from "ethers";
+import { useNotification } from "web3uikit";
 
-const formInputSchema = z
-  .object({
-    enteredText: z
-      .string()
-      .nonempty()
-  })
+const formInputSchema = z.object({
+  enteredText: z.string().nonempty(),
+});
 
-type SearchProject = z.infer<typeof formInputSchema>
+type SearchProject = z.infer<typeof formInputSchema>;
 
 export default function IndexPage() {
+  const user = useUserValue();
+  const userAddr = user ? user.uid : "";
+  const accountAddr = "0x068419813Bd03FaeeAD20370B0FB106f3A9217E4";
+  const tokenAddr = "0x07865c6e87b9f70255377e024ace6630c1eaa37f";
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<SearchProject>({
+    resolver: zodResolver(formInputSchema),
+  });
 
-  const user = useUserValue()
-  const router = useRouter()
-  const { register, handleSubmit } = useForm<SearchProject>({ resolver: zodResolver(formInputSchema) })
+  const dispatch = useNotification();
 
-  const [unreceivedDistributionBalance, setUnreceivedDistributionBalance] = useState<number | null>(null)
+  const [unreceivedDistributionBalance, setUnreceivedDistributionBalance] =
+    useState<string | null>(null);
+
+  const { data: releasableToken, isError } = useContractRead({
+    address: accountAddr,
+    abi: accountAbi,
+    functionName: "releasableToken",
+    args: [tokenAddr, userAddr],
+  });
+  console.log(typeof (releasableToken as bigint));
+  console.log(
+    ethers.utils
+      .parseUnits((releasableToken as bigint).toString(), 6)
+      .toString()
+  );
+
+  const { config } = usePrepareContractWrite({
+    address: accountAddr,
+    abi: accountAbi,
+    functionName: "withdrawToken",
+    args: [tokenAddr],
+    overrides: {
+      from: userAddr,
+    },
+  });
+  const { data, isLoading, isSuccess, write } = useContractWrite({
+    ...config,
+    onSuccess() {
+      handleWithdrawSuccess();
+    },
+    onError(error) {
+      console.log("Error", error);
+    },
+  });
 
   //get unreceived distribution balance
   useFetchEffect(async () => {
     //TODO
     //<<<Hashimoto
-    //分配金残高を取得
-
     //表示
-    setUnreceivedDistributionBalance(0)
+    setUnreceivedDistributionBalance(
+      ethers.utils
+        .parseUnits((releasableToken as bigint).toString(), 6)
+        .toString()
+    );
     //Hashimoto>>>
-  }, [])
+  }, []);
 
   const onClickSearch: SubmitHandler<SearchProject> = async (data) => {
     //get projects where invitatoin code matches
     const { data: projects } = await getProjectsWhere({
       key: KEYS.PROJECT.INVITATION_CODE,
       operation: "==",
-      value: data.enteredText
-    })
-    if (!projects || !projects.length) { return }
-    const project = projects[0]
+      value: data.enteredText,
+    });
+    if (!projects || !projects.length) {
+      return;
+    }
+    const project = projects[0];
 
     //add user.uid to member ids
-    if (!user) { return }
+    if (!user) {
+      return;
+    }
     await updateProjectArray({
       projectId: project.id,
       key: "memberIds",
       value: user.uid,
-      method: "union"
-    })
+      method: "union",
+    });
 
     //go to project page
-    router.push(PATHS.PROJECT(project.id))
-  }
+    router.push(PATHS.PROJECT(project.id));
+  };
 
   const onEnterDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key !== "Enter") { return }
-    handleSubmit(onClickSearch)
-  }
+    if (event.key !== "Enter") {
+      return;
+    }
+    handleSubmit(onClickSearch);
+  };
 
   const onClickReceiveDistribution = () => {
     //TODO
     //<<<Hashimoto
     //分配金の残高を受け取る
+    if (write) {
+      write();
+    }
 
     //表示を更新
-    setUnreceivedDistributionBalance(0)
+    setUnreceivedDistributionBalance("0");
     //Hashimoto>>>
+  };
+
+  async function handleWithdrawSuccess() {
+    dispatch({
+      type: "success",
+      message: "Withdrawing proceeds!",
+      title: "Withdrawing!",
+      position: "topR",
+    });
   }
 
   return (
     <div>
       <TabBar.Root defaultValue="projects">
         <TabBar.List>
-          <TabBar.Trigger value="projects">
-            Projects
-          </TabBar.Trigger>
-          <TabBar.Trigger value="revenue">
-            Balance
-          </TabBar.Trigger>
+          <TabBar.Trigger value="projects">Projects</TabBar.Trigger>
+          <TabBar.Trigger value="revenue">Balance</TabBar.Trigger>
         </TabBar.List>
 
         <TabBar.Content value="projects">
@@ -102,17 +162,15 @@ export default function IndexPage() {
                 {...register("enteredText")}
               />
             </label>
-            <button onClick={handleSubmit(onClickSearch)}>
-              Search
-            </button>
+            <button onClick={handleSubmit(onClickSearch)}>Search</button>
           </form>
         </TabBar.Content>
 
         <TabBar.Content value="revenue">
           <p>Unreceived distribution balance</p>
-          {unreceivedDistributionBalance !== null &&
+          {unreceivedDistributionBalance !== null && (
             <p>{`${unreceivedDistributionBalance} USDC`}</p>
-          }
+          )}
           <Spacer size={60} />
           <p>Receive distribution</p>
           <Button
@@ -125,5 +183,5 @@ export default function IndexPage() {
         </TabBar.Content>
       </TabBar.Root>
     </div>
-  )
+  );
 }
